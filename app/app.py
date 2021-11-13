2cb7d5e34286a17f800578ce8411035e27f9a900
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-11-13 11:51:44 +0530

    chore: Load Testing

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-from fastapi.params import Body, Depends
+from fastapi.params import Depends
+from fastapi.responses import FileResponse
 
 from .firebase.auth import FirebaseBearer
 from .firebase import feats, teams
 from .schemas import *
 from . import docs
+from starlette.concurrency import run_in_threadpool
 
 
 app = FastAPI(
@@ -20,19 +22,22 @@ async def root() -> dict:
 
 @app.put("/feats/generate", tags=['features'], response_model=BaseResponse[Feats]) 
 async def generateFeats(id: str = Depends(FirebaseBearer())) -> BaseResponse[Feats]:
-    val = feats.setFeat(id)
+    val = await run_in_threadpool(feats.setFeat, id)
     return BaseResponse(data=val)
 
 
 @app.post("/team", tags=['team'], response_model=BaseResponse[Team])
 async def createTeam(createTeam: CreateTeam, id: str = Depends(FirebaseBearer())) -> BaseResponse[Team]:
-    code = teams.createTeam(id, createTeam.name)
+    code = await run_in_threadpool(teams.createTeam, id, createTeam.name)
     return BaseResponse(data=Team(code=code))
 
 
 @app.put("/team/{code}", tags=['team'], response_model=EmptyResponse)
-async def joinTeam(code: int, id: str = Depends(FirebaseBearer())) -> EmptyResponse:
-    teams.joinTeam(id, code)
+async def joinTeam(code: int) -> EmptyResponse:
+    await run_in_threadpool(teams.joinTeam, id, code)
     return EmptyResponse()
 
 
+@app.get("/loaderio-bbee6adfa96244093c1f157a930fa71f/")
+async def test():
+    return FileResponse("static/loaderio-02936ad9bf9a9b15ed1ba9646d645907.txt")
