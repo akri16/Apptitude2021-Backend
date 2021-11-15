d6c9285c0517f2d06a9186b1597e03f25460e0d7
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-11-16 01:20:56 +0530

    feat: Enable team joining if created but not full

--- app/firebase/teams.py
+++ app/firebase/teams.py
@@ -6,21 +6,13 @@ import time
 from app.schemas import TeamPath
 
 
-def checkTeamChangeAllowed(id):
+def checkTeamChangeAllowed():
     if hasEventStarted():
         raise HTTPException(status_code=403, detail=constants["EVENT_STARTED"])
-
-    user = getUserDetails(id)
-
-    if 'team' in user:
-        raise HTTPException(status_code=403, detail=constants['USER_HAS_TEAM'])
 
 
 def setTeam(id, code):
     teamRef = db.reference(f"participants/{id}/team")
-
-    if teamRef.get():
-        raise HTTPException(status_code=403, detail=constants['USER_HAS_TEAM'])
 
     return teamRef.set(code)
 
@@ -30,7 +22,12 @@ def createTeam(id, name):
     teamRef = db.reference('teams')
     snapshot = teamRef.order_by_child('name').equal_to(name).get()
 
-    checkTeamChangeAllowed(id)
+    checkTeamChangeAllowed()
+
+    user = getUserDetails(id)
+    if 'team' in user:
+        raise HTTPException(status_code=403, detail=constants['USER_HAS_TEAM'])
+
 
     if len(name) > MAX_TEAM_NAME_SIZE or len(name) < MIN_TEAM_NAME_SIZE:
         raise HTTPException(status_code=400, detail=constants['INVALID_TEAM_NAME'])
@@ -38,7 +35,7 @@ def createTeam(id, name):
     if snapshot:
         raise HTTPException(status_code=403, detail=constants['TEAM_NAME_EXISTS'])
 
-    code = int(str(round(time.time_ns()))[3:14]) # Generate unique time based ids
+    code = str(round(time.time_ns()))[3:14] # Generate unique time based ids
     teamRef.child(code).set({"name": name, "members": [id]})
     setTeam(id, code)
     
@@ -46,7 +43,19 @@ def createTeam(id, name):
 
 
 def joinTeam(id, code):
-    checkTeamChangeAllowed(id)
+    checkTeamChangeAllowed()
+
+    ref = db.reference(f'teams/{code}/members/')
+
+    if not ref.get():
+        raise HTTPException(status_code=404, detail=constants["INVALID_CODE"])
+
+    user = getUserDetails(id)
+    if 'team' in user and __getTeamSize(user['team']) == 2:
+        raise HTTPException(status_code=403, detail=constants['USER_HAS_TEAM'])
+
+    if 'team' in user:
+        db.reference(f"teams/{user['team']}").delete()
 
     def __updateTeam(members):
         if len(members) == 2:
@@ -58,17 +67,15 @@ def joinTeam(id, code):
         members.append(id)
         return members
 
-    ref = db.reference(f'teams/{code}/members/')
-
-    if not ref.get():
-        raise HTTPException(status_code=404, detail=constants["INVALID_CODE"])
-
     try:
         ref.transaction(__updateTeam)
         setTeam(id, code)
     except db.TransactionAbortedError:
         raise HTTPException(status_code=500, detail=constants["INTERNAL_ERROR"])
 
+
+def __getTeamSize(code: int):
+    return len(db.reference(f'teams/{code}/members').get())
 
 def getMembers(code=None, members=None):
     if not members: 
