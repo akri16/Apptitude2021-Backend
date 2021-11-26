379bef454e73f04e214be98075a2a92f0ced93ff
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-11-26 18:32:41 +0530

    Add specific origins

--- app/app.py
+++ app/app.py
@@ -20,9 +20,11 @@ app = FastAPI(
 
 app.add_middleware(SentryAsgiMiddleware)
 
+
+origins = ["https://apptitude-pwa.vercel.app/", "https://apptitude.acmvit.in/"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
